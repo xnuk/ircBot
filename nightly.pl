42502ec6ce9377206ea47aad984e387a3e276106
Xnuk Shuman <admin@xnu.kr>
2016-11-19 22:53:29 +0900

    fix nightly.pl

--- nightly.pl
+++ nightly.pl
@@ -2,4 +2,19 @@ use strict;
 use warnings;
 
 die unless `curl --head "https://www.stackage.org/nightly"` =~ m|\nLocation: /(nightly-[\d-]+)|;
-`sed -ir 's/^(resolver: ).*\$/\\1$1/' stack.yaml`
+my $x=$1;
+my $str;
+{
+	local $/ = undef;
+	open(my $fh, "<", "stack.yaml") or die;
+	$str = <$fh>;
+	close $fh;
+}
+open(my $out, ">", "stack.yaml") or die;
+open(my $in, "<", \$str) or die;
+while(<$in>){
+	s/^(resolver: )[^\s]*/$1$x/;
+	print $out $_
+}
+close($out);
+close($in);
