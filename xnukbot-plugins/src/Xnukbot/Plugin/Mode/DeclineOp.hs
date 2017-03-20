ec77c7906ec729b7ecf0a982c302e7bfd61facc1
Xnuk Shuman <admin@xnu.kr>
2017-03-20 22:50:10 +0900

    More general DeclineOp

--- xnukbot-plugins/src/Xnukbot/Plugin/Mode/DeclineOp.hs
+++ xnukbot-plugins/src/Xnukbot/Plugin/Mode/DeclineOp.hs
@@ -5,7 +5,7 @@ import Prelude hiding (lookup)
 
 import "xnukbot" Xnukbot.Plugin (MessageT(Message), PrefixT(NickName), Plugin, Plug, hasAttr, AttrT(Protected))
 import "xnukbot" Xnukbot.Plugin.Util (deop, voice)
-import "text" Data.Text (isPrefixOf)
+import "text" Data.Text (isPrefixOf, isInfixOf)
 import "unordered-containers" Data.HashMap.Strict (lookup)
 import Control.Concurrent (forkIO)
 import Data.Maybe (isJust, fromJust)
@@ -13,9 +13,9 @@ import Data.Maybe (isJust, fromJust)
 plug :: Plug
 plug setting send (Message (Just NickName{}) "MODE" (chan : mode : nicks))
     | hasAttr chan setting "DeclineOp.disabled" = Nothing
-    | "+o" `isPrefixOf` mode && isJust nickIfMe = Just $ do
+    | "+" `isPrefixOf` mode && "o" `isInfixOf` mode && isJust nickIfMe = Just $ do
         let myNick = fromJust nickIfMe
-        forkIO $ send $ voice chan [myNick] ++ deop chan [myNick]
+        forkIO $ send $ (if "v" `isInfixOf` mode then [] else voice chan [myNick]) ++ deop chan [myNick]
         return setting
     | otherwise = Nothing
     where nickIfMe = case lookup (Protected "nickname") setting of
