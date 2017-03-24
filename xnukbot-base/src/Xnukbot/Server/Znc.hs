6a8f09728e121ce1b5295572a72cda63d77d3697
Xnuk Shuman <admin@xnu.kr>
2017-03-24 12:23:14 +0900

    use InterpolatedString and get more sexy

--- xnukbot-base/src/Xnukbot/Server/Znc.hs
+++ xnukbot-base/src/Xnukbot/Server/Znc.hs
@@ -1,16 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 module Xnukbot.Server.Znc (connect) where
 
 import "tls" Network.TLS (recvData, sendData, Context)
 import Xnukbot.Connect.TLSConnect (tlsConnect)
 import Xnukbot.Connect.Recv (parseit, parseMessage, ChunkFunc)
 import "mtl" Control.Monad.State.Lazy (runState)
-import Data.Monoid ((<>))
 import "irc" Network.IRC.Base (Message(..), showMessage)
 import Control.Monad.IO.Class (liftIO)
 import "bytestring" Data.ByteString.Lazy (fromStrict)
 import "bytestring" Data.ByteString (ByteString)
 import "network" Network.Socket (Socket)
+import "interpolatedstring-perl6" Text.InterpolatedString.Perl6 (qq)
 
 muntil :: Monad m => [m Bool] -> m Bool
 muntil [] = return False
@@ -20,29 +20,28 @@ muntil (x:xs) = do
         then return True
         else muntil xs
 
+crlf :: ByteString
+crlf = "\r\n"
+
 connect :: ByteString -> ByteString -> IO ((Context, Socket), ChunkFunc)
 connect username password = do
     nctx@(ctx, _) <- tlsConnect "znc.xnu.kr" "8152"
-    sendData ctx . fromStrict $ "NICK " <> username <> "\r\nUSER "
-                              <> username <> " "
-                              <> username <> " "
-                              <> username <> " :" <> username
-                              <> "\r\n"
+    sendData ctx . fromStrict $ [qq|NICK $username{crlf
+                                   }USER $username $username $username :$username{crlf
+                                   }|]
     let z f = do
             str <- recvData ctx
             let (msgs, f') = runState (parseit str) f
-            bool <- liftIO . muntil $ map q msgs
+            bool <- liftIO . muntil $ map login msgs
             if bool
                 then return f'
                 else z f'
-        q msg = case msg_command msg of
+
+        login msg = case msg_command msg of
             "464" | msg_params msg == [username, "Password required"] -> do
-                sendData ctx . fromStrict $ "PASS " <> username <> ":" <> password <> "\r\n"
+                sendData ctx . fromStrict $ [qq|PASS $username:$password{crlf}|]
                 print (showMessage msg)
                 return False
-            {-"PING" -> do
-                sendData ctx . fromStrict $ "PONG " <> head (msg_params msg) <> "\r\n"
-                return False-}
             "001" -> do
                 print (showMessage msg)
                 return True
